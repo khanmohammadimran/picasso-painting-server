b1ea1b91b6984cfb8852afc00946da38a7734172
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2022-05-25 01:38:50 +0600

    Single tool detail has been showed to the purchase page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -23,6 +23,13 @@ async function run() {
             const tools = await cursor.toArray();
             res.send(tools);
         });
+
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tools = await toolsCollection.findOne(query);
+            res.send(tools)
+        })
     }
     finally {
 
