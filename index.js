e93b02b64f5f1c7aaebd0a652b1f3a7ad8395abb
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2022-05-25 04:45:58 +0600

    Minimum quantity section route create

--- index.js
+++ index.js
@@ -30,6 +30,24 @@ async function run() {
             const tools = await toolsCollection.findOne(query);
             res.send(tools)
         })
+
+        // Quantity update
+        app.patch('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const newQuantity = req.body;
+            console.log(newQuantity)
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    minimumOrderQuantity: newQuantity.minimumOrderQuantity
+                }
+            };
+            const result = await toolsCollection.updateOne(filter, updatedDoc, options);
+
+            res.send(result);
+
+        })
     }
     finally {
 
