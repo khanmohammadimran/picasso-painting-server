5affe60d0b8cd005f879641192a2b51810eb7b57
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2022-05-26 10:06:10 +0600

    JWT token implement

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jsonwebtoken = require('jsonwebtoken');
 
 app.use(cors());
 app.use(express.json());
@@ -35,7 +37,8 @@ async function run() {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc, option);
-            res.send(result);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
         })
 
         app.get('/tools/:id', async (req, res) => {
